7692755cd26ccd7b5a0e66897f0404b8f5f4846c
vimalkrishnan <vimal_viyookaran@yahoo.co.in>
2019-03-03 11:57:46 +0530

    Response has been corrected for file store

--- SpearHead.FileStore.Host/SpeardHead.FileStore.Api/Controllers/FileStoreController.cs
+++ SpearHead.FileStore.Host/SpeardHead.FileStore.Api/Controllers/FileStoreController.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System.Net;
 using SpearHead.FileStore.BusinessServices.Contracts;
 using SpearHead.FileStore.Models;
 using SpearHead.FileStore.Api.Filters;
 using SpearHead.FileStore.Common.Logging;
+using SpearHead.FileStore.Api.ResponseResults;
 
 namespace SpearHead.FileStore.Api.Controllers
 {
     [RoutePrefix("filestore/api")]
-
+    //[Authorize]
     public sealed class FileStoreController : BaseController
     {
         private readonly IFileBusinessService _fileBusinessService;
@@ -21,22 +23,31 @@ namespace SpearHead.FileStore.Api.Controllers
 
         }
 
-        [HttpPut]
+        [HttpPost]
         [Route("upload")]
         [ValidateModel]
         public async Task<IHttpActionResult> UploadModel([FromBody] FileModel fileModel)
         {
             _LoggingService.Log($"Request has been recieved , correlationId {CorrelationId}");
             int id = await _fileBusinessService.UploadFile(fileModel);
-            return Ok(id);
+            return new StatusCodeResult<object>(id, HttpStatusCode.Created);
         }
 
+        [HttpGet]
         [Route("download/{id}")]
         public async Task<IHttpActionResult> Get([FromUri] int id)
         {
             _LoggingService.Log($"Request has been recieved , correlationId {CorrelationId}");
-            return Ok(await _fileBusinessService.Dowload(id));
+            return new StatusCodeResult<FileModel>(await _fileBusinessService.Dowload(id), HttpStatusCode.OK);
         }
 
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IHttpActionResult> Delete([FromUri] int id)
+        {
+            _LoggingService.Log($"Request has been recieved , correlationId {CorrelationId}");
+            await _fileBusinessService.Delete(id);
+            return new StatusCodeResult<string>("Deleted", HttpStatusCode.Accepted);
+        }
     }
 }
---
+++ SpearHead.FileStore.Host/SpeardHead.FileStore.Api/ResponseResults/StatusCodeResult.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SpearHead.FileStore.Api.ResponseResults
+{
+    public sealed class StatusCodeResult<T> : IHttpActionResult where T : class
+    {
+        private readonly T _content;
+        private readonly HttpStatusCode _httpStatusCode;
+
+        public StatusCodeResult(T content, HttpStatusCode httpStatusCode)
+        {
+            _content = content ?? throw new ArgumentNullException(nameof(content));
+            _httpStatusCode = httpStatusCode;
+        }
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return Task.FromResult(new HttpResponseMessage(_httpStatusCode)
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(_content))
+            });
+        }
+    }
+}
